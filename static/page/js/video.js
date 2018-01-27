0a15d4dd7c4dd983b4bd7f4194521763159d23f3
sivanZhang <445080336@qq.com>
2018-01-27 16:28:49 +0800

    picture

--- static/page/js/video.js
+++ static/page/js/video.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
     /* 视频弹出框 */
     $('.video-first').on("click",function(){
+        var videoSrc = $(this).attr('data-video');
         $('.v-wrap').show();
+        $('.v-wrap').children('video').attr('src',videoSrc);
     });
     $('.close').on("click",function(){
-        $(this).parent('v-wrap').hide();
+        $('.v-wrap').children('video').attr('src','');
+        $(this).parent('.v-wrap').hide();
     });
     
     $('.video-list').on("click",function(){
-        /* var videoSrc = $(this). */
+        var videoSrc = $(this).attr('data-video');
         $('.v-wrap').show();
+        $('.v-wrap').children('video').attr('src',videoSrc);
     });
-
-    
 })
