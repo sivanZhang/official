0da344eb47cc5f892a5f7964c52ea52f2f8c4401
sivanZhang <445080336@qq.com>
2018-01-28 19:08:52 +0800

    优化

--- static/page/js/picture.js
+++ static/page/js/picture.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
-    /* 视频弹出框 */
+    /* 图片弹出框 */
     $('.pic-list img').on("click",function(){
         $('.high_definition').show();
         $('#pic_wall').attr('src','');
-    var imgSrc=$(this).attr('src')
+        var imgSrc=$(this).attr('src');
         $('#pic_wall').attr('src',imgSrc);
+        /* 下一张 */
+        var imgIndex=$(this).index();
+        $('#more').on("click",function(){
+            var imgIndex=imgIndex+1;
+            var nextSrc=$(".pic-list img").index(imgIndex).attr('src');
+            $('#pic_wall').attr('src',nextSrc); 
+        })
     });
     /* 关闭按钮 */
     $('.close').on("click",function(){
