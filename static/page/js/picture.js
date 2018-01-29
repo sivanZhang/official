56470059ac017d807d6b574f8afc0c474ba0dce8
sivanZhang <445080336@qq.com>
2018-01-29 14:40:25 +0800

    图片点击BUG

--- static/page/js/picture.js
+++ static/page/js/picture.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
     /* 图片弹出框 */
-    $('.pic-list img').on("click",function(){
+    $('.active-img').on("click",function(){
         $('.high_definition').show();
         $('#pic_wall').attr('src','');
         var imgSrc=$(this).attr('src');
         $('#pic_wall').attr('src',imgSrc);
         /* 下一张 */
-        var imgIndex=$(this).index();
+       /*  var imgIndex=$(this).index();
         $('#more').on("click",function(){
             var imgIndex=imgIndex+1;
             var nextSrc=$(".pic-list img").index(imgIndex).attr('src');
             $('#pic_wall').attr('src',nextSrc); 
-        })
+        }) */
     });
     /* 关闭按钮 */
     $('.close').on("click",function(){
