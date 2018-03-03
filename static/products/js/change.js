adf959495e04ec63fc400d9c04b6a8659d839af0
jeawy <461882709@qq.com>
2018-03-03 17:48:50 +0800

    添加页面删除

--- static/products/js/change.js
+++ static/products/js/change.js
@@ -91,3 +91,38 @@ $(".ta-wrap input").on('keyup input', function (event) {
 });
 
 ///以下是修改product时用到的js
+
+//  发表+存稿按钮    >>> 点击事件
+$('.btn-delete ').click(function () {
+    //loading样式
+    var options = {
+        theme: "sk-doc",
+        message: '提交中...',
+        backgroundColor: "#000",
+        textColor: "white"
+    };
+    HoldOn.open(options);//loadding函数调用 
+    var product = $('#productid');
+      
+    data = {
+        'method': 'delete',
+        'id':product.val(),
+        'csrfmiddlewaretoken': getCookie('csrftoken'),
+    };
+  
+    var html = '<div class="alert alert-danger" role="alert">####</div>';
+    $.ajax({
+        type: 'post',
+        url: '/product/products/',
+        data: data,
+        success: function (result) {
+            HoldOn.close();
+            location.href="/product/products/";
+        },
+        error: function () {
+            HoldOn.close();
+            // 500
+            alert('server is down!')
+        }
+    })
+});
