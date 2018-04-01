656349a5a0d9441632ed4d6538830a2f27c4fbc1
sivanZhang <445080336@qq.com>
2018-04-01 17:15:25 +0800

    移动端通用

--- static/js/m_base.js
+++ static/js/m_base.js
@@ -14,35 +14,7 @@ function getCookie(name) {
     }
     return cookieValue;
 }
-/* 
- *返回按钮
- */
-$(document).ready(function() {
-    $('i.back').click(function() {
-        history.back();
-    });
-    $('.menu-list td').mouseenter(function(){
-        $(this).find('.point').addClass('orange-point');
-    })
-    $('.menu-list td').mouseleave(function(){
-        $(this).find('.point').removeClass('orange-point');
-    })
-    /* 点击页面回收悬浮菜单 */
-    jQuery(document).click(function () {
-        jQuery('.btn-list li').slideUp();
-        $('#show-btn').children().removeClass('img-close');
-    })
-    jQuery('#show-btn').click(function (e) {
-        jQuery('.btn-list li').slideToggle();
-       $(this).children().toggleClass('img-close');
-        e.stopPropagation();
-    })
-    jQuery(document).click(function () {
-        jQuery('#user_login_nav').slideUp();
-    })
 
-    
-});
 /* 验证 */
 //手机号码验证
 function verify_phone(phone) {
@@ -71,39 +43,14 @@ function isEmail(email) {
     var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     return regex.test(email);
 }
-
-$(window).resize(function() {
-    var leftValue =$('.point:first').offset().left;
-    $('.bottom-line').css('left',leftValue+'px');
-});
-/* 点击显示qq群二维码 */
-$('#qq_group').on('click',function(){
-    $('.code-wrap').eq(0).fadeIn(300);
-})
-$('#wei_group').on('click',function(){
-    $('.code-wrap').eq(1).fadeIn(300);
-})
-$('.close_code').on('click',function(){
-    $(this).parents('.code-wrap').fadeOut(300);
-})
-$('.code-bg').on('click',function(){
-    $(this).parents('.code-wrap').fadeOut(300);
-})
-
-/* 
- *菜单下边线的样式
- */
-$(window).load(function () {
-
-    var leftValue = $('.point:first').offset().left,
-        lineWidth = $('body').width()-leftValue;
-    topValue = $('.point:first').offset().top;
-    $('.bottom-line').css('width',lineWidth+ 'px').css('left', leftValue + 'px').css('top', topValue + 2 + 'px');
-})
-$(window).resize(function() {
-    var leftValue =$('.point:first').offset().left;
-    $('.bottom-line').css('left',leftValue+'px');
-});
-$('#link').on('click',function(){
-    $('nav ul').slideToggle();
+$('.group-title').each(function(){
+    $(this).on('click',function(){
+        var display = $(this).next('.links').css('display');
+        $(this).next('.links').slideToggle();
+        if( display=='none'){
+            $(this).children('.show-btn').addClass('rota');
+        }else{
+            $(this).children('.show-btn').removeClass('rota');
+        }
+    })
 })
