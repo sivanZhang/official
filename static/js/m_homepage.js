7552ee527adf29a7c8ded75fbf6bc090baedd2f1
sivanZhang <445080336@qq.com>
2018-04-05 18:35:52 +0800

    后台报错BUG

---
+++ static/js/m_homepage.js
@@ -0,0 +1,16 @@
+$(document).ready(function(e){
+var height = window.innerHeight;
+$('.swiper-container').height(height);
+$('.content').height(height);
+ var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        // Enable debugger
+        debugger: true,
+        autoplay : 5000,
+        loop : true
+    });
+});
+
